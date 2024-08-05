ff7389166dc2f6e6b60e88f517a44d58743cf98a
Sean Clancy <141309945+SeanClancy84@users.noreply.github.com>
2024-08-05 20:17:06 +0800

    Update remove-google-redirection-lite.user.js
    修复Safari失效

--- remove-google-redirection-lite.user.js
+++ remove-google-redirection-lite.user.js
@@ -19,18 +19,34 @@
 // @run-at         document-end
 // ==/UserScript==
 
-        const aTags = document.querySelectorAll('a');
+//console.log('Script started');
 
-        // Loop through all <a> tags
-        aTags.forEach(a => {
-            // Remove data-ved attribute if it exists
-            if (a.hasAttribute('data-ved')) {
-                a.removeAttribute('data-ved');
-            }
+function processATags() {
+    const aTags = document.querySelectorAll('a');
+    aTags.forEach(a => {
+        //console.log('Processing a tag:', a);
 
-            // Remove data-sb attribute if it exists
-            if (a.hasAttribute('data-sb')) {
-                a.removeAttribute('data-sb');
-            }
-        });
+        // Remove data-ved attribute if it exists
+        if (a.hasAttribute('data-ved')) {
+            a.removeAttribute('data-ved');
+        }
 
+        // Remove data-sb attribute if it exists
+        if (a.hasAttribute('data-sb')) {
+            a.removeAttribute('data-sb');
+        }
+
+        // Extract URL from href and replace href with the URL parameter
+        const href = a.getAttribute('href');
+        const urlMatch = href && href.match(/[?&]url=([^&]+)/);
+        if (urlMatch && urlMatch[1]) {
+            const newUrl = decodeURIComponent(urlMatch[1]);
+            //console.log('Replacing href with:', newUrl);
+            a.setAttribute('href', newUrl);
+        }
+    });
+    //console.log('Processed all a tags');
+}
+
+// Initial processing of a tags
+processATags();
